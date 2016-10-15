9d9c9f70cb16b65f59ec11ed34e8b8857c5766e1
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-10-15 18:39:14 +0700

    Create bai4.c

---
+++ bai4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+ void main()
+
+{
+
+    float a,b,cv,s;
+
+    printf("chieu dai hinh chu nhat la: \n");
+
+    scanf("%f",&a);
+    
+    printf("chieu rong hinh chu nhat la:\n");
+    
+    scanf("%f",&b);
+
+    cv=(a+b)*2;
+
+    s=a*b;
+
+    printf("Chu vi hinh chu nhat la: cv=%0.2f",cv);
+
+    printf("Dien tich hinh vuong la: s=%0.2f",s);
+
+}
