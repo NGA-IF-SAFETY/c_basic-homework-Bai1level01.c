2c163bcdd70398a9b4405c32f83caaab274c8b31
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-10-15 18:50:25 +0700

    Create bai5.c

---
+++ bai5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+    float a,b,c,d,chuVi,dienTich;
+
+    printf("Nhap gia tri do dai day lon cua hinh thang: \n");
+
+    scanf("%f",&a);
+
+    printf ("nhap gia tri do dai day be cua hinh thang: \n");
+
+    scanf ("%f",&b);
+
+    printf ("nhap gia tri do dai canh ben thu nhat: \n");
+
+    scanf ("%f",&c);
+
+    printf ("nhap gia tri do dai canh ben thu hai: \n");
+
+    scanf ("%f",&d);
+
+    printf ("nhap gia tri do dai duong cao: \n");
+
+    scanf ("%f",&h);
+    
+    chuVi=a+b+c+d; 
+     
+    printf("Chu vi hinh thang la:chuVi=%0.2f",chuVi);
+
+    printf("Dien tich hinh vuong la: dienTich=%0.2f",dienTich);
+
+}
