148bad567feba314ba46941549607336eda86c2a
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-10-15 20:14:12 +0700

    Create bai6.c

---
+++ bai6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main ()
+{
+float r,h,v;
+printf("nhap gia tri ban kinh mặt đáy hinh trụ: r\n");
+scanf("%f",&r);
+printf("nhap gia tri duong cao cua hinh tru: \n");
+scantf("%f",&h);
+v=3,14*r*r*h;
+printf("the tivh hinh tru la: %0.2f",v);
+}
